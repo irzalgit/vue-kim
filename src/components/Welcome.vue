e59964c618dababcf2cd3d834ab09490cd5a6176
mathjsx <irzalpa23@outlook.com>
2024-12-21 12:03:53 +0700

    Initial commit for vue-kim

---
+++ src/components/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="welcome">
+    <h1>Welcome to Vue.js Project: vue-kim!</h1>
+    <p>This project is a simple Vue.js application deployed to GitHub Pages.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+};
+</script>
+
+<style scoped>
+.welcome {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
