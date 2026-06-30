addd90e426325c2d21fb5b4945fe85ce1bd66d7d
Irzal Achmad <irzalpa20@gmail.com>
2026-06-30 08:11:51 +0700

    Add React application under app directory

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import Navigation from "./sections/Navigation";
+import Hero from "./sections/Hero";
+import Curriculum from "./sections/Curriculum";
+import CinematicVision from "./sections/CinematicVision";
+
+export default function App() {
+  return (
+    <div style={{ background: "#0a0a0a", minHeight: "100vh" }}>
+      <Navigation />
+      <Hero />
+      <Curriculum />
+      <CinematicVision />
+    </div>
+  );
+}
