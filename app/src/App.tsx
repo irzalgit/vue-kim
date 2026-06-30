eb9a977a41e5697dc969aa26ce46810c5d50bced
Irzal Achmad <irzalpa20@gmail.com>
2026-06-30 14:26:06 +0700

    tambah komppnentgooyer

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,7 @@ import Hero from "./sections/Hero";
 import Curriculum from "./sections/Curriculum";
 import CinematicVision from "./sections/CinematicVision";
 import AlumniArchives from './sections/AlumniArchives';
+import Footer from './sections/Footer';
 export default function App() {
   return (
     <div style={{ background: "#0a0a0a", minHeight: "100vh" }}>
@@ -11,6 +12,7 @@ export default function App() {
       <Curriculum />
       <CinematicVision />
        <AlumniArchives />
+        <Footer />
     </div>
   );
 }
